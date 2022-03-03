75a4fdfdfb796fa798be11cbd09ba6028fd0e536
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 14:14:05 -0700

    Adddb queries

---
+++ queries.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const connection = require('./knexfile')[process.env.NODE_ENV || 'development'];
+const database = require('knex')(connection);
+
+module.exports = {
+  getAllQuestions(request) {
+    // get 20 questions at random
+    let query = database('questions').select('*').orderByRaw('RANDOM()').limit(20);
+    return query
+  },
+  getQuestionsByCategory(request) {
+    const category = request.query.category;
+    return database('questions').where('category', category).limit(20);
+  },
+  postUserQuestion(question) {
+      return database('questions').insert(question, 'id');
+  }
+
+}
